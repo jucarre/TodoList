aa8c4d5f0bd13c80aec2f06f61747edd98ffc04e
jucarre <juliencarre06@gmail.com>
2019-10-17 20:45:27 +0200

    Add Test phpunit Controller and coverage repport

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -1,10 +1,83 @@
 <?php
 
-
 namespace App\Tests\Controller;
 
 
-class SecurityControllerTest
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
 {
+    private $client;
+
+    protected function setUp() : void
+    {
+        $this->client = self::createClient();
+    }
+
+    public function testLogin(): void
+    {
+
+        $crawler = $this->client->request('GET', '/login');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $this->assertEquals(1, $crawler->filter('input[name="username"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="password"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="_csrf_token"]')->count());
+
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['username'] = 'admin';
+        $form['password'] = 'root';
+        $this->client->submit($form);
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $this->assertSame('Se déconnecter', $crawler->filter('a.pull-right.btn.btn-danger')->text());
+    }
+
+    public function testLoginFailUsername(): void
+    {
+        $crawler = $this->client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['username'] = 'boby';
+        $form['password'] = '';
+        $this->client->submit($form);
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $this->assertSame('L\'utilsateur n\'existe pas.', $crawler->filter('div.alert-danger')->text());
+    }
+
+    public function testLoginFailPassword(): void
+    {
+
+        $crawler = $this->client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Se connecter')->form();
+
+        $form['username'] = 'admin';
+        $form['password'] = 'azertyui';
+        $this->client->submit($form);
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $this->assertSame('Mot de passe incorrect', $crawler->filter('div.alert-danger')->text());
+    }
+
+    public function tearDown() : void
+    {
+        $this->client = null;
+        $crawler = null;
+    }
+
 
 }
--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -4,7 +4,101 @@
 namespace App\Tests\Controller;
 
 
-class UserControllerTest
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class UserControllerTest extends WebTestCase
 {
+
+    private $client;
+
+    protected function setUp(): void
+    {
+        $this->client = self::createClient();
+    }
+
+    public function loginWithAdmin(): void
+    {
+        $crawler = $this->client->request('GET', '/login');
+        $form = $crawler->selectButton('Se connecter')->form();
+        $this->client->submit($form, ['username' => 'admin', 'password' => 'root']);
+    }
+
+    public function testListAction(): void
+    {
+        $this->loginWithAdmin();
+
+        $this->client->request('GET', '/users');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testCreateAction(): void
+    {
+        $this->loginWithAdmin();
+
+        $crawler = $this->client->request('GET', '/users/create');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $this->assertSame('Nom d\'utilisateur', $crawler->filter('label[for="user_username"]')->text());
+        $this->assertSame('Mot de passe', $crawler->filter('label[for="user_password_first"]')->text());
+        $this->assertSame('Adresse email', $crawler->filter('label[for="user_email"]')->text());
+
+        $this->assertEquals(1, $crawler->filter('input[name="user[username]"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="user[password][first]"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="user[password][second]"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="user[email]"]')->count());
+        $this->assertEquals(2, $crawler->filter('input[name="user[roles][]"]')->count());
+
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['user[username]'] = 'boby';
+        $form['user[password][first]'] = 'azerty';
+        $form['user[password][second]'] = 'azerty';
+        $form['user[email]'] = 'newUser@example.org';
+        $form['user[roles][0]']->tick();
+        $this->client->submit($form);
+
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('div.alert-success')->count());
+
+
+    }
+
+    public function testEditeAction(): void
+    {
+        $this->loginWithAdmin();
+
+        $crawler = $this->client->request('GET', '/users/4/edit');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+
+        $this->assertSame('Nom d\'utilisateur', $crawler->filter('label[for="user_username"]')->text());
+        $this->assertSame('Mot de passe', $crawler->filter('label[for="user_password_first"]')->text());
+        $this->assertSame('Adresse email', $crawler->filter('label[for="user_email"]')->text());
+
+        $this->assertEquals(1, $crawler->filter('input[name="user[username]"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="user[password][first]"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="user[password][second]"]')->count());
+        $this->assertEquals(1, $crawler->filter('input[name="user[email]"]')->count());
+        $this->assertEquals(2, $crawler->filter('input[name="user[roles][]"]')->count());
+
+        $form = $crawler->selectButton('Modifier')->form();
+        $form['user[username]'] = 'bobynight';
+        $form['user[password][first]'] = 'root';
+        $form['user[password][second]'] = 'root';
+        $form['user[email]'] = 'newUser@example.org';
+        $form['user[roles][0]']->tick();
+        $this->client->submit($form);
+
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('div.alert-success')->count());
+
+    }
+
 
 }
